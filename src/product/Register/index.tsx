919b398b2f2a30dd0d1863c06ecaf12d532b3b32
rogemaster <rlaaudwn000@naver.com>
2024-05-31 23:33:37 +0900

    상품등록 페이지 작업

--- src/product/Register/index.tsx
+++ src/product/Register/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback, useState } from 'react';
 import loadable from '@loadable/component';
+import {ProductCountryOriginOptions, ProductStatusOptions} from "@src/shared/ProductOptions";
 
 const Navigation = loadable(() => import('@src/components/Navigation'));
 
@@ -10,26 +11,87 @@ interface IProductRegister {
 // {/* 상품명 판매상태 판매가격 상품정보고시 판매정책(과세여부 등) 옵션 배송정책 메인이미지, 상세설명 */}
 
 const ProductRegisterIndex = ({ pageName }: IProductRegister) => {
+  const [selectedStatusOption, setSelectedStatusOption] = useState('');
+  const [isOptionStatusSelected, setIsOptionStatusSelected] = useState<boolean>(false);
+
+  const [selectedCountryOption, setSelectedCountryOption] = useState('');
+  const [isOptionCountrySelected, setIsOptionCountrySelected] = useState<boolean>(false);
+
+  const onSelectProductStatus = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setIsOptionStatusSelected(true);
+    setSelectedStatusOption(value);
+  }, []);
+
+  const onSelectCountryOrigin = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setIsOptionCountrySelected(true);
+    setSelectedCountryOption(value);
+  }, []);
+
   return (
     <>
       <Navigation pageName={pageName} />
       <div className="rounded-sm border border-gray-100 bg-white shadow">
         <form action="#">
           <div className="p-6">
-            <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
+            <div className="mb-4 flex flex-col gap-6 xl:flex-row">
               <div className="w-full xl:w-1/2">
                 <label className="mb-2.5 block text-black">상품명</label>
                 <input
                   type="text"
-                  className="w-full rounded border border-gray-200 bg-transparent py-2 px-4 text-black text-[13px] outline-none transition focus:border-blue-600 active:border-blue-600 disabled:cursor-default disabled:bg-white"
+                  className="w-full rounded border border-gray-200 bg-transparent py-2 px-4 text-black outline-none transition focus:border-blue-600 active:border-blue-600 disabled:cursor-default disabled:bg-white"
                 />
               </div>
               <div className="w-full xl:w-1/2">
                 <label className="mb-2.5 block text-black">브랜드명</label>
                 <input
                   type="text"
-                  className="w-full rounded border border-gray-200 bg-transparent py-2 px-4 text-black text-[13px] outline-none transition focus:border-blue-600 active:border-blue-600 disabled:cursor-default disabled:bg-white"
+                  className="w-full rounded border border-gray-200 bg-transparent py-2 px-4 text-black outline-none transition focus:border-blue-600 active:border-blue-600 disabled:cursor-default disabled:bg-white"
                 />
+              </div>
+            </div>
+
+            {/* 판매상타 */}
+            <div className="mb-4 flex flex-col gap-6 xl:flex-row">
+              <div className="w-full xl:w-1/2">
+                <label className="mb-2.5 block text-black">판매상태</label>
+                <div className="relative z-20 bg-transparent">
+                  <select
+                    name="product_status"
+                    id="product_status"
+                    value={selectedStatusOption}
+                    onChange={onSelectProductStatus}
+                    className={`relative z-20 w-full appearance-none rounded border border-gray-200 bg-transparent py-3 px-5 outline-none transition focus:border-blue-600 active:border-blue-600 ${isOptionStatusSelected ? 'text-black' : ''}`}
+                  >
+                    {ProductStatusOptions.map((option) => (
+                      <>
+                        <option value='' className="text-gray-500">선택</option>
+                        <option value={option.key} className="text-gray-500">{option.name}</option>
+                      </>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="w-full xl:w-1/2">
+                <label className="mb-2.5 block text-black">원산지</label>
+                <div className="relative z-20 bg-transparent">
+                  <select
+                    name="product_status"
+                    id="product_status"
+                    value={selectedCountryOption}
+                    onChange={onSelectCountryOrigin}
+                    className={`relative z-20 w-full appearance-none rounded border border-gray-200 bg-transparent py-3 px-5 outline-none transition focus:border-blue-600 active:border-blue-600 ${isOptionCountrySelected ? 'text-black' : ''}`}
+                  >
+                    {ProductCountryOriginOptions.map((option) => (
+                      <>
+                        <option value='' className="text-gray-500">선택</option>
+                        <option value={option.key} className="text-gray-500">{option.name}</option>
+                      </>
+                    ))}
+                  </select>
+                </div>
               </div>
             </div>
           </div>
---
+++ src/shared/ProductOptions.ts
@@ -0,0 +1,25 @@
+export const ProductStatusOptions = [
+  { key: '001', name: '판매중' },
+  { key: '002', name: '대기중' },
+  { key: '003', name: '품절' },
+  { key: '004', name: '판매중지' },
+];
+
+export const ProductDeliveryOptions = [
+  { key: '001', name: '무료배송' },
+  { key: '002', name: '선결제' },
+  { key: '003', name: '착불' },
+  { key: '004', name: '조건부배송' },
+];
+
+export const ProductCountryOriginOptions = [
+  { key: 'KO', name: '대한민국' },
+  { key: 'USA', name: '미국' },
+  { key: 'JP', name: '일본' },
+  { key: 'CH', name: '중국' },
+  { key: 'RU', name: '러시아' },
+  { key: 'CH', name: '중국' },
+  { key: 'UK', name: '영국' },
+  { key: 'VI', name: '베트남' },
+  { key: 'CH', name: 'AU' },
+];
